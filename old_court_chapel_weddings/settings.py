26a157dd644f7445705f9ef4479bf54d668939d2
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-10-03 16:11:50 +0100

    deployment

--- old_court_chapel_weddings/settings.py
+++ old_court_chapel_weddings/settings.py
@@ -140,28 +140,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-# )
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# # TO RUN LOCALLY HAVE THESE TWO UNCOMMENTED #
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+   os.path.join(BASE_DIR, "static"),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# TO RUN LOCALLY HAVE THESE TWO UNCOMMENTED #
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 
 
