fc2950c6a1df79a3b10be699d6dc42ff6cd88f27
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-10-04 15:12:33 +0100

    whitenoise added

--- old_court_chapel_weddings/settings.py
+++ old_court_chapel_weddings/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+from whitenoise import WhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'old_court_chapel_weddings.urls'
@@ -153,6 +155,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # TO RUN LOCALLY HAVE THESE TWO UNCOMMENTED #
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
