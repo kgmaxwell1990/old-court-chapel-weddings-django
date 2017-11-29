79006f7d397b65f9374777d175197b6028021fc0
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-11-29 09:28:02 +0000

    settings

--- old_court_chapel_weddings/settings.py
+++ old_court_chapel_weddings/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get('DEBUG', False)
 
 
 
-ALLOWED_HOSTS = ['old-court-chapel-weddings.herokuapp.com', '127.0.0.1', 'www.oldcourtchapelweddings.co.uk', 'www.oldcourtchapelweddings.co.uk.herokudns.com']
+ALLOWED_HOSTS = ['old-court-chapel-weddings.herokuapp.com', '127.0.0.1', 'www.oldcourtchapelweddings.co.uk', 'www.oldcourtchapelweddings.co.uk.herokudns.com', 'm.oldcourtchapelweddings.co.uk']
 INTERNAL_IPS = ['127.0.0.1']
 
 # console
